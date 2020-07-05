f8828bf6e9883d94b0771a2145dd892729906fd3
Prakhar Goenka <prakhar.goinka@gmail.com>
2020-07-05 19:41:24 +0530

    client-app: Add axios support

--- client-app/components/Home.js
+++ client-app/components/Home.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 
 export default class Home extends Component{
+  state = {
+    userData: null
+  }
 
   componentDidMount() {
-    this.props.navigation.setOptions({title: 'Welcome to Walmart!'})
+    axios.get('https://reqres.in/api/users/2')
+    .then(res => {
+        this.props.navigation.setOptions({
+          title: `Welcome ${res.data.data.first_name}!`
+        })
+        this.setState({
+          userData: res.data
+      })
+    })
+    .catch(console.log)    
   }
 
   render() {
