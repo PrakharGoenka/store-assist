73048d2689c87a97929c2e4a108e7ad8758e585f
Rudresh Pratap Singh <rudreshsingh999@gmail.com>
2020-07-05 23:09:39 +0530

    client-app : Add grid view to Assist

--- client-app/components/Assist.js
+++ client-app/components/Assist.js
@@ -1,23 +1,64 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
+import { Text, View, StyleSheet, Dimensions, ScrollView } from 'react-native';
+const { width } = Dimensions.get("window");
 
-export default class Assist extends Component{
+export default class Assist extends Component {
+  _isMounted = false;
+  state = {
+    tileDimensions : [],
+    tiles : []
+  }
+
+  componentDidMount() {
+    this._isMounted = true;
+    this.setState ({
+      tileDimensions : calcTileDimensions(width, 2),
+      tiles : 'Lorem Ipsum Dolor Sit Amet to the very end'.split(' ')
+    })
+  }
+
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <StatusBar style="auto" />
-        <Text>Screen 2</Text>
-      </View>
+      <ScrollView>
+        <View style={styles.container}>
+          {this.state.tiles.map(i => Item({...this.state.tileDimensions, text: i}))}     
+        </View>
+      </ScrollView>
     );
-  };  
+  }
 }
+
+const Item = ({size, margin, text}) => (
+    <View key={text} 
+      style={[styles.item, {width: size, height: size, marginHorizontal: margin}]} 
+      onStartShouldSetResponder={() => alert('You click by View')}>
+      <Text style={styles.itemText}>{text}</Text>
+    </View>
+)
+
+const calcTileDimensions = (deviceWidth, tpr) => {
+  const margin = deviceWidth / (tpr * 10);
+  const size = (deviceWidth - margin * (tpr * 2)) / tpr;
+  return { size, margin };
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+     justifyContent: "flex-start", flexDirection: "row", flexWrap: "wrap", marginTop: 30
   },
+  item: {
+    backgroundColor: 'black',  
+     alignSelf: "flex-start",
+     alignItems: 'center',
+     justifyContent: 'center',
+     marginBottom: 20
+  },
+  itemText: {
+    color: 'white',
+    fontSize: 20
+  }
 });
