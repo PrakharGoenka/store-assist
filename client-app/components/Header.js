72cfd2b34353854905c9bbc7a1e436cf48b93bfa
Prakhar Goenka <prakhar.goinka@gmail.com>
2020-07-06 20:49:36 +0530

    client-app: Add custom header component

---
+++ client-app/components/Header.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Entypo } from '@expo/vector-icons';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}> {this.props.title} </Text>
+        <Entypo.Button 
+          name="shopping-cart" 
+          size={24} 
+          color="orange" 
+          backgroundColor="white"
+          onPress={() => 
+            this.props.navigation.navigate('Cart')
+          }
+        />
+      </View> 
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignContent: "space-between",
+    alignItems: "center",
+    justifyContent: "space-between"
+  },
+  title: {
+    fontSize: 20, 
+    fontWeight: "bold"
+  },
+})
