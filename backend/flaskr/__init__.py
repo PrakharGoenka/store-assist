f5fc7dd0052bff581a763ea0e093605f1e6d2bc8
Shreyam Kumar <kumarshreyam0409@gmail.com>
2020-07-06 12:11:56 +0530

    Add an init endpoint

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -60,17 +60,24 @@ def create_app(test_config=None):
         btid = db.StringField(required=True, unique = True)
         section = db.StringField(max_length = 100)
 
-    #a simple page that says hello
-    # @app.route('/hello')
-    # def hello():
-    #     bt = BTsects(btid="007", section="sect_name").save()
-    #     bt2 = BTsects(btid="0409", section="sect_name2").save()
-    #     # item1 = Item(url='url', name='name1', desc='desc')
-    #     # item2 = Item(url='url', name='name2', desc='desc')
-    #     # sect = Section(loc='loc', name='sect_name2', items=[item1, item2])
-    #     # sect.save()
-    #     # tmp = SuperSection(name='super', sections=['sect_name', 'sect_name']).save()
-    #     return 'Hello, World!'
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        camera1 = Item(url="https://images-eu.ssl-images-amazon.com/images/I/51UHoxzInpL._AC_SX184_.jpg",name="Canon EOS 1500D 24.1", desc = "Canon EOS 1500D 24.1 Digital SLR Camera (Black) with EF S18-55 is II Lens, 16GB Card and Carry Case")
+        camera2 = Item(url="https://images-eu.ssl-images-amazon.com/images/I/41+-LjzbkuL._AC_SX184_.jpg",name="Sony Alpha ILCE5100L 24.3MP", desc = "Sony Alpha ILCE5100L 24.3MP Digital SLR Camera (Black) with 16-50mm Lens with Free Case (Bag)")
+        camera3 = Item(url="https://images-na.ssl-images-amazon.com/images/I/415ZSUQ2erL.jpg",name="Canon EOS 200D", desc = "Canon EOS 200D II 24.1MP Digital SLR Camera + EF-S 18-55mm is STM Lens + EF-S 55-250mm is STM Lens (Black)")
+        camera4 = Item(url="https://images-eu.ssl-images-amazon.com/images/I/51QiHopSU8L._AC_SX184_.jpg",name="Sony Alpha ILCE 6000Y 24.3 MP ", desc = "Sony Alpha ILCE 6000Y 24.3 MP Mirrorless Digital SLR Camera with 16-50 mm and 55-210 mm Zoom Lenses (APS-C Sensor, Fast Auto Focus, Eye AF) - Black")
+        cameraSection = Section(loc="C1", name="Cameras", items=[camera1, camera2, camera3, camera4]).save()
+
+        phone1 = Item(url="https://images-eu.ssl-images-amazon.com/images/I/71OxJeyywSL._AC._SR360,460.jpg",name="Samsung Galaxy M31", desc = "Space Black, 6GB RAM, 128GB Storage")
+        phone2 = Item(url="https://images-eu.ssl-images-amazon.com/images/I/61ACGAKmw3L._AC._SR360,460.jpg",name="Redmi Note 8 Pro",desc="Halo White, 6GB RAM, 128GB Storage with Helio G90T Processor")
+        phone3 = Item(url="https://images-eu.ssl-images-amazon.com/images/I/415IOkCUtgL._AC._SR180,230.jpg",name="Vivo U10",desc="Electric Blue, 5000 mAH 18W Fast Charge Battery, 3GB RAM, 32GB Storage")
+        phone4 = Item(url="https://images-eu.ssl-images-amazon.com/images/I/31Z2ee9oYQL._AC._SR180,230.jpg",name="Nokia 105 2019",desc="Single SIM, Black")
+        phoneSection = Section(loc="P1",name="Mobiles", items=[phone1, phone2, phone3, phone4]).save()
+
+        elecSect = SuperSection(name="Electronics",sections=['cameraSection', 'phoneSection']).save()
+
+        return 'Hello, World!'
 
     @app.route('/bt/<btid>', methods=['GET'])
     def getSectionBT(btid):
@@ -98,7 +105,6 @@ def create_app(test_config=None):
     @app.route('/section/sub/<sectName>', methods = ['GET'])
     def getSectionItems(sectName):
         sect = Section.objects(name=sectName).first()
-        print(sect.items)
         itemlist = []
         for item in sect.items:
             obj = {
