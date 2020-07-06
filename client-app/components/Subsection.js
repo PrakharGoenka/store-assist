952d641c95cb4a07981092092c2fb189a56a3fb9
Prakhar Goenka <prakhar.goinka@gmail.com>
2020-07-06 17:50:24 +0530

    client-app: Implement fetch data for subsection

--- client-app/components/Subsection.js
+++ client-app/components/Subsection.js
@@ -1,43 +1,41 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import { StyleSheet, Text, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import ItemCarousel from './ItemCarousel';
 
 export default class Subsection extends Component{
   state = {
-    userData: null
+    subsection: null,
+    items: []
+  }
+
+  async componentDidMount() {
+    const { subsection } = this.props.route.params
+    this.props.navigation.setOptions({
+      title: `Explore ${subsection}`
+    })
+    try {
+      const res = await axios.get(
+        `http://192.168.1.103:5000/section/sub/${subsection}`
+      )
+      const { items } = res.data
+      this.setState ({
+        subsection,
+        items
+      })
+    } catch(error) {
+      console.log(error)
+    }
   }
 
   render() {
-    const carouselItems = [
-      {
-          title:"Item 1",
-          text: "Text 1",
-      },
-      {
-          title:"Item 2",
-          text: "Text 2",
-      },
-      {
-          title:"Item 3",
-          text: "Text 3",
-      },
-      {
-          title:"Item 4",
-          text: "Text 4",
-      },
-      {
-          title:"Item 5",
-          text: "Text 5",
-      },
-    ]
-    
     return (
       <View style={styles.container}>
         <StatusBar style="auto" />
         <View style={styles.carousel}>
           <Text> Top Picks</Text>
-          <ItemCarousel carouselItems={carouselItems}/>
+          <ItemCarousel carouselItems={this.state.items}/>
         </View>
       </View>
     );
